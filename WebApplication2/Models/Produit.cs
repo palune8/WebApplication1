e43b63da051db7187bfd065c9cd39e1017bfd9e1
Palune <Palune@MAC-DSI-1016.electricite.sn>
2020-02-18 15:08:09 +0000

    Ajoutez des fichiers projet.

---
+++ WebApplication2/Models/Produit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Produit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
